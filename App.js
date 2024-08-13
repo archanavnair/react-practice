791f53f926b7288e86264f022cd62526494e8812
archanavnair <archanavnair90@gmail.com>
2024-08-13 18:34:11 -0400

    day-2: functional component

--- App.js
+++ App.js
@@ -1,32 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/**
- * <div id="parent">
- *    <div id="child">
- *      <h1>I'm an H1 tag</h1>
- *      <h2>I'man h2 tag</h2>
- *    </div>
- * </div>
- *
- */
+/********Starting from scratch*************/
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag 🙅"),
-    React.createElement("h2", {}, "I'm an h2 tag "),
-  ])
-); //due to this complexity we have JSX
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
+//JSX (transpiled before it reaches the JS engine) which is done by Parcel with the help of Babel library
 
-// console.log(heading); //heafng is an react object
+//React Functional Component
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React Title Component
+  </h1>
+);
+
+//Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Namaste React Functional Component 🚀</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);
